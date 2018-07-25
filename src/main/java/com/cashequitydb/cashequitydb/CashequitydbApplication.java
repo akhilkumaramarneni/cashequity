ee2f39bf76691fe8ea08bf7be10ac1d4c4cb5848
akhilkumaramarneni <akhilkumaramarneni@gmail.com>
2018-07-25 13:04:38 +0530

    initial commit

---
+++ src/main/java/com/cashequitydb/cashequitydb/CashequitydbApplication.java
@@ -0,0 +1,12 @@
+package com.cashequitydb.cashequitydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashequitydbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashequitydbApplication.class, args);
+	}
+}
